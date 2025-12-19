05062a7bfc5b7c5158eddb20eff92a0e5e2da8ad
JesCorAvi <jesuscorderoavila1@gmail.com>
2025-12-19 13:32:48 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // <--- ESTA LÍNEA ES LA SOLUCIÓN
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    unoptimized: true, // Esto es necesario para 'export' (ya lo tenías, perfecto)
   },
 }
 
